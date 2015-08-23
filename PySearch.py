4251a52ae2343d314ffd8f96c066f6570e938c89
slickness <randyh1981@gmail.com>
2015-08-23 10:59:40 -0400

    added ability to change browser and launch a tab

--- PySearch.py
+++ PySearch.py
@@ -1,4 +1,9 @@
 __author__ = 'randy'
 
+import webbrowser
+new = 2 # open in new tab if an obtion
+x = webbrowser.get('chromium-browser')
+x.open("www.google.com/search?q=mlb scores ",new=new)
+
 
 print "test"
