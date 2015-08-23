d767965712b04a3becb8cdca1e1fc229a855f533
slickness <randyh1981@gmail.com>
2015-08-23 15:31:00 -0400

    fixed the look of the gui and added comments

--- searchQui.py
+++ searchQui.py
@@ -1,4 +1,4 @@
-from PyQt4 import QtCore, QtGui, Qt
+from PyQt4 import QtGui
 import sys
 import PySearch
 #gui to search a browser and an engine
@@ -6,38 +6,52 @@ import PySearch
 class SearchWindowClass(QtGui.QWidget):
     def __init__(self):
         super(SearchWindowClass,self).__init__()
-        self.setGeometry(50,50,200,200)
+        self.setGeometry(50,50,240,125)
         self.setWindowTitle("Web Searcher")
         self.home()
+        self.setFixedSize(240,125)
     def home(self):
-        self.LabelStatus = QtGui.QLabel("Search any search engine",self)
-        self.LabelStatus.setAlignment(QtCore.Qt.AlignCenter)
-        self.Engine = QtGui.QComboBox(self)
-        self.Engine.move(5,50)
-        self.Engine.addItems(['Google','Yahoo','Bing'])
+
+        #label for what combo boxes do
+        self.labelB = QtGui.QLabel("BROWSER",self)
+        self.labelB.move(50,5)
+
+        self.labelE = QtGui.QLabel("ENGINE",self)
+        self.labelE.move(170,5)
+
+        #combo box to choose browser to search, only displays browsers available to use
         self.browserChoice = QtGui.QComboBox(self)
         self.browserChoice.move(5,25)
+
+        #getting available search engines
         Avail = PySearch.getAvailableBrowser()
         for x in Avail:
             self.browserChoice.addItem(x)
-        # self.LabelA = QtGui.QLabel("Enter Search String",self)
-        # self.LabelA.move(5,75)
+
+        #combo box to choose which search engine to use Google, Yahoo, or Bing
+        self.Engine = QtGui.QComboBox(self)
+        self.Engine.move(160,25)
+        self.Engine.addItems(['Google','Yahoo','Bing'])
+
+        #text box to take search string
         self.SearchString =QtGui.QLineEdit('Enter required search' ,self)
-        self.SearchString.setFixedWidth(175)
-        self.SearchString.move(5,100)
+        self.SearchString.setFixedWidth(230)
+        self.SearchString.move(5,60)
 
+        #search button
         self.Button = QtGui.QPushButton('click to search',self)
-
-        self.Button.move(20,125)
+        self.Button.move(65,90)
         self.Button.clicked.connect(self.Searcher)
+
     def Searcher(self):
+        #collects engine, browser, and search string and sends it to searcher
         SearchString = str(self.SearchString.text())
         myBrowser = str(self.browserChoice.currentText())
         myEngine = str(self.Engine.currentText())
         PySearch.goIndSearch(myBrowser,SearchString,myEngine)
 
 if __name__=="__main__":
-    #print PySearch.urls
+
     app = QtGui.QApplication(sys.argv)
     SearchWindow = SearchWindowClass()
     SearchWindow.show()
