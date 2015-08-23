f31e237c3c69d197b52e539ff21bf62deb9142b2
slickness <randyh1981@gmail.com>
2015-08-23 12:45:46 -0400

    let user pick browser and serch string

--- PySearch.py
+++ PySearch.py
@@ -1,8 +1,14 @@
-__author__ = 'randy'
-
 import webbrowser
+import os
 
+#the beginning urls for three search engines
+gUrl = "www.google.com/search?q="
+yUrl =  "search.yahoo.com/search?p="
+bUrl =  "www.bing.com/search?q="
 
+urls = [gUrl,yUrl,bUrl]
+
+new = 2 # open in new tab if an obtion
 def getAvailableBrowser():
     #using the browsers in webbwoser determine which ones are availabe on the system
     posBrowser = ['mozilla','firefox','netscape','galeon','epiphany','skipstone','kfmclient',
@@ -17,12 +23,26 @@ def getAvailableBrowser():
         except:
             pass
     return availableBrowsers
-new = 2 # open in new tab if an obtion
+
+
 #x = webbrowser.get('chromium-browser') #make an instance of chromium browser pos add a schoose option
 #x.open("www.google.com/search?q=mlb scores ",new=new) # google search uses q =
-
+#x.open("www.bing.com/search?q=mlb scores ",new=new) # bing search uses q =
 #x.open("search.yahoo.com/search?p=mlb scores",new=new) # yahoo search uses p =
+def goSearch(browser,searchString):
+    for searcher in urls:
+        webbrowser.get(browser).open(searcher + searchString)
+if __name__=="__main__":
+    AvBrowser = getAvailableBrowser()
+    try:
+        os.system('clear')
+    except:
+        pass
+    y = 0
+    for x in AvBrowser:
+        print str(y) + ". " + x
+        y = y + 1
 
-AvBrowser = getAvailableBrowser()
-
-print AvBrowser
+    browser = raw_input("please enter the number to your browser: ")
+    searchString = raw_input("please input search: ")
+    goSearch(AvBrowser[int(browser)],searchString)
